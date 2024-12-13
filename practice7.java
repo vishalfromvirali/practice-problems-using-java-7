80a940a8eaa0ae709194379d3e2a74be89b6126a
vishal p <vishalfromvirali@gmail.com>
2024-12-14 00:40:33 +0530

    Add files via upload

---
+++ practice7.java
@@ -0,0 +1,10 @@
+
+class practice7{
+    public static void main(String[]args){
+        String name="vishal";
+        StringBuilder names=new StringBuilder(name);
+        names.reverse();
+        System.out.println(names);
+        
+    } 
+}
